86eeb21226cb8fba8d7fc768a66df367ed3f39c6
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-01-04 22:35:39 +0100

    feat: add the ability to change the colors
    changes:
    - changing the foreground color is now possible
    - changing the background color is now possible
    - quite some refactoring
    - small fixes applied
    
    closes #8

--- src/WIFI.QR-Code.Builder/Windows/MainWindow.xaml.cs
+++ src/WIFI.QR-Code.Builder/Windows/MainWindow.xaml.cs
@@ -1,10 +1,5 @@
 ﻿using System.Windows;
-using System.Windows.Media.Imaging;
 
-using Net.Codecrete.QrCodeGenerator;
-
-using WIFI.QRCode.Builder.Interfaces;
-using WIFI.QRCode.Builder.Services;
 using WIFI.QRCode.Builder.ViewModels;
 
 namespace WIFI.QRCode.Builder.Windows;
@@ -14,42 +9,15 @@ namespace WIFI.QRCode.Builder.Windows;
 /// </summary>
 public partial class MainWindow : Window
 {
-  private readonly IQrCodeService _qrCodeService;
-  private readonly MainViewModel _qrCodeModel;
+  private readonly MainViewModel _mainViewModel;
 
+  /// <summary>
+  /// Initializes an instance of <see cref="MainWindow"/> class.
+  /// </summary>
   public MainWindow()
   {
-    _qrCodeService = new QrCodeService();
-    _qrCodeModel = new();
-    _qrCodeModel.PropertyChanged += (s, e) => OnQrCodeModelChanged();
-
+    _mainViewModel = new();
+    DataContext = _mainViewModel;
     InitializeComponent();
-    DataContext = _qrCodeModel;
-    UpdateQrCode();
   }
-
-  private void OnQrCodeModelChanged()
-    => UpdateQrCode();
-
-  private void UpdateQrCode()
-  {
-    QrCode qrCode = QrCode.EncodeText(_qrCodeModel.Text, _qrCodeModel.ErrorCorrection);
-    QrCodeImage.Source = _qrCodeService.CreateDrawing(qrCode, 192, _qrCodeModel.BorderWidth);
-  }
-
-  private void CopyButton_Click(object sender, RoutedEventArgs e)
-  {
-    // put the QR code on the clipboard as a bitmap
-    QrCode qrCode = QrCode.EncodeText(_qrCodeModel.Text, _qrCodeModel.ErrorCorrection);
-    BitmapSource bitmap = _qrCodeService.CreateBitmapImage(qrCode, 20, _qrCodeModel.BorderWidth);
-    DataObject dataObject = new();
-    dataObject.SetData(DataFormats.Bitmap, bitmap);
-    Clipboard.SetDataObject(dataObject);
-  }
-
-  private void AboutItem_Click(object sender, RoutedEventArgs e)
-  { }
-
-  private void QuitItem_Click(object sender, RoutedEventArgs e)
-    => Environment.Exit(1);
 }
