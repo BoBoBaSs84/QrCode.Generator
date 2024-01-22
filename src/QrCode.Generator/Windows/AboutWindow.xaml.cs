a1997b39d5e266ea033a694a5a601cac524bbdf4
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-01-22 17:42:04 +0100

    feat: about window or control
    chnages:
    - some window refactoring
    - about view model added
    - other stuff changed
    
    closes #22

---
+++ src/QrCode.Generator/Windows/AboutWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+using QrCode.Generator.ViewModels;
+
+namespace QrCode.Generator.Windows;
+
+/// <summary>
+/// Interaction logic for AboutWindow.xaml
+/// </summary>
+public partial class AboutWindow : Window
+{
+  /// <summary>
+  /// Initializes an instance of <see cref="AboutWindow"/> class.
+  /// </summary>
+  public AboutWindow(AboutViewModel viewModel)
+  {
+    DataContext = viewModel;
+    InitializeComponent();
+  }
+}
