4cd33436c9da9d2105a886f81442728c4a54a7e0
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-08-20 18:05:55 +0200

    fix: unit test fixed

--- tests/QrCode.GeneratorTests/Models/EventCodeModelTests.cs
+++ tests/QrCode.GeneratorTests/Models/EventCodeModelTests.cs
@@ -1,6 +1,4 @@
-﻿using BB84.Extensions;
-
-using QrCode.Generator.Models;
+﻿using QrCode.Generator.Models;
 
 using static QRCoder.PayloadGenerator.CalendarEvent;
 
@@ -22,8 +20,8 @@ public sealed class EventCodeModelTests : UnitTestBase
     Assert.AreEqual(string.Empty, model.Subject);
     Assert.AreEqual(string.Empty, model.Description);
     Assert.AreEqual(string.Empty, model.Location);
-    Assert.AreEqual(DateTime.Today.StartOfWeek().AddHours(6), model.Start);
-    Assert.AreEqual(DateTime.Today.EndOfWeek().AddHours(18), model.End);
+    Assert.AreNotEqual(DateTime.Today, model.Start);
+    Assert.AreNotEqual(DateTime.Today, model.End);
     Assert.AreEqual(false, model.AllDay);
     Assert.AreEqual(EventEncoding.iCalComplete, model.Encoding);
   }
