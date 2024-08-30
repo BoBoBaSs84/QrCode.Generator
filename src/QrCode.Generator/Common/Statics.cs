6fcde1b1209555d6cb8b656478e13b4cc2645367
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-08-30 21:16:57 +0200

    feat: common statics added

---
+++ src/QrCode.Generator/Common/Statics.cs
@@ -0,0 +1,19 @@
+﻿namespace QrCode.Generator.Common;
+
+/// <summary>
+/// The application statics.
+/// </summary>
+internal static class Statics
+{
+  /// <summary>
+  /// The default template path for loading and saving.
+  /// </summary>
+  public static string DefaultTemplatePath
+    => Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+  /// <summary>
+  /// The default template file filter for loading and saving.
+  /// </summary>
+  public static string TemplateFileFilter
+    => "Template files (*.json)|*.json";
+}
