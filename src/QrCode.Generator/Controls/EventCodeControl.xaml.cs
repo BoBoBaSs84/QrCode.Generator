93a9ebc80b8da553ae2d051e19367932d41b2912
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-09-01 19:59:42 +0200

    feat: qr code export implemented
    closes #95

--- src/QrCode.Generator/Controls/EventCodeControl.xaml.cs
+++ src/QrCode.Generator/Controls/EventCodeControl.xaml.cs
@@ -6,6 +6,7 @@ using BB84.Extensions;
 using Microsoft.Win32;
 
 using QrCode.Generator.Common;
+using QrCode.Generator.Extensions;
 using QrCode.Generator.ViewModels;
 
 namespace QrCode.Generator.Controls;
@@ -65,6 +66,25 @@ public partial class EventCodeControl : UserControl
     {
       _viewModel.SavePath = fileDialog.FileName;
       _viewModel.SaveTemplateCommand.Execute(_viewModel.Model);
+    }
+  }
+
+  private void OnExportButtonClick(object sender, RoutedEventArgs e)
+  {
+    SaveFileDialog fileDialog = new()
+    {
+      Title = "Export qr code ...",
+      Filter = _viewModel.ExportType.GetFilterFromType(),
+      InitialDirectory = _viewModel.ExportPath,
+      FileName = $"Export.{_viewModel.ExportType.ToString().ToLowerInvariant()}"
+    };
+
+    bool? result = fileDialog.ShowDialog();
+
+    if (result.HasValue && result.Value.IsTrue())
+    {
+      _viewModel.ExportPath = fileDialog.FileName;
+      _viewModel.ExportCommand.Execute();
     }
   }
 }
--- src/QrCode.Generator/Controls/GiroCodeControl.xaml.cs
+++ src/QrCode.Generator/Controls/GiroCodeControl.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 using BB84.Extensions;
 
 using Microsoft.Win32;
 
 using QrCode.Generator.Common;
+using QrCode.Generator.Extensions;
 using QrCode.Generator.ViewModels;
 
 namespace QrCode.Generator.Controls;
@@ -64,6 +66,25 @@ public partial class GiroCodeControl : UserControl
     {
       _viewModel.SavePath = fileDialog.FileName;
       _viewModel.SaveTemplateCommand.Execute(_viewModel.Model);
+    }
+  }
+
+  private void OnExportButtonClick(object sender, RoutedEventArgs e)
+  {
+    SaveFileDialog fileDialog = new()
+    {
+      Title = "Export qr code ...",
+      Filter = _viewModel.ExportType.GetFilterFromType(),
+      InitialDirectory = _viewModel.ExportPath,
+      FileName = $"Export.{_viewModel.ExportType.ToString().ToLowerInvariant()}"
+    };
+
+    bool? result = fileDialog.ShowDialog();
+
+    if (result.HasValue && result.Value.IsTrue())
+    {
+      _viewModel.ExportPath = fileDialog.FileName;
+      _viewModel.ExportCommand.Execute();
     }
   }
 }
