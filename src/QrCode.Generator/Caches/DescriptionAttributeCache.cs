e0d8a08f26956f4e06b9f9c82313f814b9e75d05
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-09-01 19:02:22 +0200

    feat: description attribute cache and extension method added

---
+++ src/QrCode.Generator/Caches/DescriptionAttributeCache.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace QrCode.Generator.Caches;
+
+/// <summary>
+/// The description attribute cache class.
+/// </summary>
+/// <typeparam name="T">Should be a <see cref="Enum"/> value.</typeparam>
+internal static class DescriptionAttributeCache<T> where T : struct, IComparable, IFormattable, IConvertible
+{
+  private static readonly Dictionary<T, string> Descriptions;
+
+  /// <summary>
+  /// Initializes a instance of the description attribute cache class.
+  /// </summary>
+  static DescriptionAttributeCache()
+  {
+    Descriptions = [];
+
+    Type type = typeof(T);
+    foreach (T value in Enum.GetValues(type).Cast<T>())
+    {
+      string valueName = value.ToString()!;
+      Descriptions.Add(value, type.GetMember(valueName)[0].GetCustomAttribute<DescriptionAttribute>()?.Description ?? valueName);
+    }
+  }
+
+  /// <summary>
+  /// Returns the description of the <see cref="Enum"/> <paramref name="value"/> from the cache.
+  /// </summary>
+  /// <param name="value">The enum type value.</param>
+  /// <returns>The enumerator description.</returns>
+  internal static string GetDescription(T value)
+    => Descriptions[value];
+}
