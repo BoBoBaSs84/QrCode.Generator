854564d70c6ac74e4a988f0894aba2329331efa6
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2025-12-03 16:41:48 +0100

    fix: various fixes applied due to breaking changes

---
+++ src/QR-Code.API/Extensions/SwaggerUIOptionsExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace QRCode.API.Extensions;
+
+/// <summary>
+/// Represents extension methods for configuring <see cref="SwaggerUIOptions"/>
+/// </summary>
+public static class SwaggerUIOptionsExtensions
+{
+  /// <summary>
+  /// Configures the specified <see cref="SwaggerUIOptions"/> with default settings.
+  /// </summary>
+  /// <param name="options">The <see cref="SwaggerUIOptions"/> instance to configure.</param>
+  /// <returns>
+  /// The same <see cref="SwaggerUIOptions"/> instance, so that additional configuration can be chained.
+  /// </returns>
+  internal static SwaggerUIOptions ConfigureSwaggerUIOptions(this SwaggerUIOptions options)
+  {
+    options.JsonSerializerOptions = new JsonSerializerOptions();
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+
+    return options;
+  }
+}
