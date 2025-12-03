049a79ed5a31ea49bfc473d3fe073977888d6ddb
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2025-12-03 16:41:48 +0100

    fix: various fixes applied due to breaking changes

---
+++ src/QR-Code.API/Extensions/SwaggerOptionsExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Swashbuckle.AspNetCore.Swagger;
+
+namespace QRCode.API.Extensions;
+
+/// <summary>
+/// Represents extension methods for configuring <see cref="SwaggerOptions"/>
+/// </summary>
+public static class SwaggerOptionsExtensions
+{
+  /// <summary>
+  /// Configures the specified <see cref="SwaggerOptions"/> with default settings for OpenAPI version 2.0.
+  /// </summary>
+  /// <param name="options">The <see cref="SwaggerOptions"/> instance to configure.</param>
+  /// <returns>
+  /// The same <see cref="SwaggerOptions"/> instance, so that additional configuration can be chained.
+  /// </returns>
+  internal static SwaggerOptions ConfigureSwaggerOptions(this SwaggerOptions options)
+  {
+    options.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi2_0;
+    options.RouteTemplate = "swagger/{documentName}/swagger.json";
+
+    return options;
+  }
+}
