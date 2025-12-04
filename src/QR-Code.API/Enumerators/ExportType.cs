042d902e202dd8ae667de5358d8624563aecc98d
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2025-12-04 18:37:09 +0100

    feat: Add support for multiple QR code export formats
    Introduced the `ExportType` property in `CodeRequestBase` to allow specifying the desired QR code export format. Added the `ExportType` enumerator to define supported formats (BMP, PNG, JPEG, SVG, PDF) with descriptions.
    
    Refactored the `GenerateBarCode` method in `QRCodeService` to dynamically handle export formats using a new `GetExportBytesAndMimeType` method. Added helper methods for generating QR codes in each format, leveraging the `QRCoder` library.
    
    Defined MIME types for each export format and updated namespaces and imports. Improved code modularity and extensibility by separating format-specific logic into distinct methods.

---
+++ src/QR-Code.API/Enumerators/ExportType.cs
@@ -0,0 +1,42 @@
+﻿// -----------------------------------------------------------------------------
+// Copyright:	Robert Peter Meyer
+// License:		MIT
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+// -----------------------------------------------------------------------------
+using System.ComponentModel;
+
+namespace QRCode.API.Enumerators;
+
+/// <summary>
+/// The export type enumerator.
+/// </summary>
+public enum ExportType
+{
+  /// <summary>
+  /// Windows Bitmap Graphics
+  /// </summary>
+  [Description("Windows Bitmap Graphics")]
+  BMP = 0,
+  /// <summary>
+  /// Portable Network Graphics
+  /// </summary>
+  [Description("Portable Network Graphics")]
+  PNG = 1,
+  /// <summary>
+  /// Joint Photographic Experts Group
+  /// </summary>
+  [Description("Joint Photographic Experts Group")]
+  JPEG = 2,
+  /// <summary>
+  /// Scalable Vector Graphics
+  /// </summary>
+  [Description("Scalable Vector Graphics")]
+  SVG = 3,
+  /// <summary>
+  /// Portable Document Format
+  /// </summary>
+  [Description("Portable Document Format")]
+  PDF = 4
+}
